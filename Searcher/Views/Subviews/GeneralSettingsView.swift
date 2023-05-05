619dda413e2a144e7a097339260755db6af1e93d
Alexey Nikandrov <alexey@Alexeys-Mac-mini.local>
2023-05-05 23:53:40 +0300

    [#1] init setting view
    added 2 tabs with subviews

---
+++ Searcher/Views/Subviews/GeneralSettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  GeneralSettingsView.swift
+//  Searcher
+//
+//  Created by Alexey Nikandrov on 05/05/2023.
+//
+
+import SwiftUI
+
+struct GeneralSettingsView: View {
+    var body: some View {
+        Text("GeneralSettingsView")
+    }
+}
+
+struct GeneralSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeneralSettingsView()
+    }
+}
